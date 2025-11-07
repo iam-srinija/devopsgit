a174f73cd0b6b9f393e4006cbc97a8ae5023a83e
iam-srinija <immadisettysaisrinija@gmail.com>
2025-11-07 18:55:20 +0530

    updated the java code

--- app.java
+++ app.java
@@ -2,3 +2,20 @@ class helloworld{
 	public static void maim(String []args);
 	System.out.println("hello");
 }
+//dev2 code to pull the code to local
+public class Sample
+{
+   public static void main(String[] args)
+   {
+      // Demonstrate println
+      System.out.println("This string is displayed on one line.");
+      System.out.println("This string appears on the second line.");
+      System.out.println(); // The third line displayed is blank
+
+      // Demonstrate print
+      System.out.print("Four ");
+      System.out.print("and twenty ");
+      System.out.println("blackbirds");
+      System.out.println("Baked in a pie.");
+   } // End main
+} // End Sample
