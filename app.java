39a45a9468f35d201b1565fbfa9b86cbea1cf0e9
iam-srinija <immadisettysaisrinija@gmail.com>
2025-11-07 19:09:26 +0530

    newly updated the java file

--- app.java
+++ app.java
@@ -11,11 +11,5 @@ public class Sample
       System.out.println("This string is displayed on one line.");
       System.out.println("This string appears on the second line.");
       System.out.println(); // The third line displayed is blank
-
-      // Demonstrate print
-      System.out.print("Four ");
-      System.out.print("and twenty ");
-      System.out.println("blackbirds");
-      System.out.println("Baked in a pie.");
    } // End main
 } // End Sample
